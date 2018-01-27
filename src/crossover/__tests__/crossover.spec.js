5a661264b7cc3bf9686e45395531c10019dab6e7
Angel Paredes Barato <angel.p.barato@gmail.com>
2018-01-27 12:21:13 +0100

    Two fix cross point

--- src/crossover/__tests__/crossover.spec.js
+++ src/crossover/__tests__/crossover.spec.js
@@ -1,9 +1,10 @@
 import { mockRandom, resetMockRandom } from 'jest-mock-random';
+import { forAll } from 'testier';
 import IndividualBuilder from '../../population/__tests__/builders/IndividualBuilder';
-import onePointCrossOver from '../crossover';
+import { onePointCrossOver, twoPointCrossOver } from '../crossover';
 
 describe('onePointCrossOver', () => {
-    it('performs a cross over over one point in the gene list ', () => {
+    it('performs a cross over on one point in the gene list ', () => {
         const ind1 = new IndividualBuilder().withFeatures([3, 2, 1]).build();
         const ind2 = new IndividualBuilder().withFeatures([4, 5, 6]).build();
         mockRandom(2 / 3);
@@ -19,5 +20,66 @@ describe('onePointCrossOver', () => {
             }),
         ]);
         resetMockRandom();
+    });
+});
+
+describe('twoPointCrossOver', () => {
+    it('performs a cross over on two points in the gene list ', () => {
+        const ind1 = new IndividualBuilder().withFeatures([6, 5, 4, 3, 2, 1]).build();
+        const ind2 = new IndividualBuilder().withFeatures([12, 11, 10, 9, 8, 7]).build();
+        mockRandom([2 / 6, 5.9 / 6]);
+
+        const children = twoPointCrossOver(ind1, ind2);
+
+        expect(children).toEqual([
+            expect.objectContaining({
+                features: [6, 5, 10, 9, 8, 1],
+            }),
+            expect.objectContaining({
+                features: [12, 11, 4, 3, 2, 7],
+            }),
+        ]);
+        resetMockRandom();
+    });
+    it('performs a cross over on two points in the gene list ', () => {
+        const ind1 = new IndividualBuilder().withFeatures([6, 5, 4, 3, 2, 1]).build();
+        const ind2 = new IndividualBuilder().withFeatures([12, 11, 10, 9, 8, 7]).build();
+        mockRandom([2 / 6, 2 / 6]);
+
+        const children = twoPointCrossOver(ind1, ind2);
+
+        expect(children).toEqual([
+            expect.objectContaining({
+                features: [6, 5, 10, 3, 2, 1],
+            }),
+            expect.objectContaining({
+                features: [12, 11, 4, 9, 8, 7],
+            }),
+        ]);
+        resetMockRandom();
+    });
+    forAll([
+        [0 / 3, 0 / 3],
+        [1 / 3, 1 / 3],
+        [2 / 3, 2 / 3],
+        [2.9 / 3, 2.9 / 3],
+    ], ([randomFixPoint1, randomFixPoint2]) => {
+        it(`performs a cross over on two points on 3 gene list with ${[randomFixPoint1, randomFixPoint2]}`, () => {
+            const ind1 = new IndividualBuilder().withFeatures([1, 2, 3]).build();
+            const ind2 = new IndividualBuilder().withFeatures([4, 5, 6]).build();
+            mockRandom([randomFixPoint1, randomFixPoint2]);
+
+            const children = twoPointCrossOver(ind1, ind2);
+
+            expect(children).toEqual([
+                expect.objectContaining({
+                    features: [1, 5, 3],
+                }),
+                expect.objectContaining({
+                    features: [4, 2, 6],
+                }),
+            ]);
+            resetMockRandom();
+        });
     });
 });
--- src/crossover/crossover.js
+++ src/crossover/crossover.js
@@ -1,6 +1,6 @@
 import generateIndividual from '../population/Individual';
 
-export default function ({ features: features1, fitnessFunction }, { features: features2 }) {
+export function onePointCrossOver({ features: features1, fitnessFunction }, { features: features2 }) {
     const fixCrossPoint = Math.round(Math.random() * (features1.length - 1));
 
     const featuresChild1 = [
@@ -10,6 +10,39 @@ export default function ({ features: features1, fitnessFunction }, { features: f
     const featuresChild2 = [
         ...features2.slice(0, fixCrossPoint),
         ...features1.slice(fixCrossPoint),
+    ];
+    return [
+        generateIndividual(featuresChild1, fitnessFunction),
+        generateIndividual(featuresChild2, fitnessFunction),
+    ];
+}
+
+export function twoPointCrossOver({ features: features1, fitnessFunction }, { features: features2 }) {
+    let fixCrossPoint1 = Math.round(Math.random() * (features1.length - 1));
+    let fixCrossPoint2 = Math.round(Math.random() * (features1.length - 1));
+
+    if (fixCrossPoint1 > fixCrossPoint2) {
+        [fixCrossPoint1, fixCrossPoint2] = [fixCrossPoint2, fixCrossPoint1];
+    } else if (fixCrossPoint1 === fixCrossPoint2) {
+        if (fixCrossPoint2 === features1.length - 1) {
+            fixCrossPoint1 -= 1;
+        } else if (fixCrossPoint2 === 0) {
+            fixCrossPoint1 = 1;
+            fixCrossPoint2 = 2;
+        } else {
+            fixCrossPoint2 += 1;
+        }
+    }
+
+    const featuresChild1 = [
+        ...features1.slice(0, fixCrossPoint1),
+        ...features2.slice(fixCrossPoint1, fixCrossPoint2),
+        ...features1.slice(fixCrossPoint2),
+    ];
+    const featuresChild2 = [
+        ...features2.slice(0, fixCrossPoint1),
+        ...features1.slice(fixCrossPoint1, fixCrossPoint2),
+        ...features2.slice(fixCrossPoint2),
     ];
     return [
         generateIndividual(featuresChild1, fitnessFunction),
