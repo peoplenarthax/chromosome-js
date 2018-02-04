90b64d137d60dda9421cfe05d9399278849df8dc
Angel Paredes Barato <angel.p.barato@gmail.com>
2018-02-04 18:39:38 +0100

    intInRangeMutation

--- src/mutation/__tests__/mutation.spec.js
+++ src/mutation/__tests__/mutation.spec.js
@@ -1,16 +1,36 @@
 import { mockRandomForEach } from 'jest-mock-random';
-import flipMutation from '../mutation';
+import { forAll } from 'testier';
+import { flipMutation, intInRangeMutation } from '../mutation';
 import IndividualBuilder from '../../population/__tests__/builders/IndividualBuilder';
 
 describe('Mutation', () => {
     describe('flipMutation', () => {
         mockRandomForEach([0.01, 0.5, 0.5]);
-        it('flips randomly bits if under passed threshold ', () => {
+        it('flips randomly the feature bits if random under the passed threshold', () => {
             const individual = IndividualBuilder.binaryIndividual([0b0, 0b1, 0b1]);
 
             const actual = flipMutation(0.05, individual);
 
-            expect(actual.features).toEqual([0b1, 0b1, 0b1]);
+            expect(actual).toEqual([0b1, 0b1, 0b1]);
+        });
+    });
+
+    describe('intInRangeMutation', () => {
+        mockRandomForEach([0.01, 0.6, 0.5, 0.5]);
+        forAll([
+            { range: [0, 10], expected: [6, 5, 10] },
+            { range: [3, 10], expected: [7, 5, 10] },
+            { range: [-5, 10], expected: [4, 5, 10] },
+        ], ({ range, expected }) => {
+            it(`mutates to an int between ${range}`, () => {
+                const individual = new IndividualBuilder()
+                    .withFeatures([2, 5, 10])
+                    .build();
+
+                const actual = intInRangeMutation(0.05, range, individual);
+
+                expect(actual).toEqual(expected);
+            });
         });
     });
 });
--- src/mutation/mutation.js
+++ src/mutation/mutation.js
@@ -1,9 +1,17 @@
-// TODO: It returns a new individua
+function randomBetween(lower, upper) {
+    const width = upper - lower;
 
-export default function flipMutation(mutationProbability, individual) {
-    const newIndividual = individual;
-    newIndividual.features = individual.features
+    return Math.round(Math.random() * width) + lower;
+}
+
+export function flipMutation(mutationProbability, { features }) {
+    return features
         .map(gene => ((Math.random() < mutationProbability) ? gene ^ 1 : gene));
+}
 
-    return individual;
+export function intInRangeMutation(mutationProbability, range, { features }) {
+    return features
+        .map(feature => ((Math.random() < mutationProbability)
+            ? randomBetween(...range)
+            : feature));
 }
