a13776ef0eff45b15e1e8f47faa5d092e41ff2c5
Angel Paredes Barato <angel.barato@izettle.com>
2018-11-14 21:14:36 +0100

    Add @babel/core for build

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const del = require('del');
 
-gulp.task('build', ['clean-dist'], () =>
+gulp.task('build', ['clean-dist'], () => {
     gulp.src(['index.js', 'src/**/*.js', '!src/**/__tests__/*'])
         .pipe(babel())
-        .pipe(gulp.dest('./dist')));
+        .pipe(gulp.dest('./'));
+});
 
 gulp.task('clean-dist', () => del(['dist']));
