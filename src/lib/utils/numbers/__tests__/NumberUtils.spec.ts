24c16cb1caa7a0c1e044442f4be758b764c3ce07
Angel Paredes Barato <angel.barato@izettle.com>
2019-03-26 20:18:03 +0100

    isDecimal to typescript

--- src/lib/utils/numbers/__tests__/NumberUtils.spec.ts
+++ src/lib/utils/numbers/__tests__/NumberUtils.spec.ts
@@ -2,14 +2,15 @@ import { forAll } from 'testier';
 import isDecimal from '../NumberUtils';
 
 describe('isDecimal', () => {
-    forAll([
+    forAll<{value: number, expected: boolean}>([
         { value: 0, expected: false },
         { value: 0.01, expected: true },
         { value: 0.34, expected: true },
         { value: 123, expected: false },
         { value: 1.11, expected: true },
         { value: 123123123, expected: false },
-    ], ({ value, expected }) => it(`asserts if ${value} is decimal or not`, () => {
+    ], ({ value, expected }) =>
+        it(`asserts if ${value} is decimal or not`, () => {
         const actual = isDecimal(value);
 
         expect(actual).toBe(expected);
